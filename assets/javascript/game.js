9d482822e000d4885ff7d715ac1875d8915ce5b1
dicimafran <cimafran16@gmail.com>
2018-05-27 20:51:16 -0400

    it works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,49 +8,52 @@
 
     Process
         Each click generates random number 1-12
-            
-        // Beginning conditions
-            function gameStart() {
-                compNumber()
-                console.log(randNumber)
-            }
-
-
-        //Input
-            $("#red").click(function() {
-                console.log("Ye clicked me.");
-            });
 */
 
 // Variables
-    var randNumber = ""; 
-    var userCrystals= "";
-
-
+    var userScore= 0;
+    var wins= 0;
+    var losses= 0;
 
 // That document.ready thing to make the page load first.
     $(document).ready(function() {
 
-// General RNG function
+//General functions
     function rngTime(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }  
-        // Random Number RNG
-            $("#randNumber").html (console.log (rngTime(19, 120)))
 
-        // Crystal RNG function
-            $("img").click(function() {
-                console.log (rngTime(1, 12))
-            });
-
-
-
-
-
-
-      
-       
-
+// General RNG function followed by sub-RNGs
     
+    // Random Number RNG
+        var randNumber = rngTime(19, 120); 
+        $("#randNumber").html(randNumber)
+  
+    // Crystal RNG function and userscore
+        $("img").click(function() {
+            cp= rngTime(1, 12)
+            console.log("The cp is "+ cp)
+            userScore= userScore + cp
+            $("#userScore").html(userScore)
+
+    // Win and lose conditions
+            
+        if (randNumber == userScore) {
+            wins= wins+= 1;
+            console.log(wins)
+            $("#winTally").html(wins)
+            userScore = 0;
+            $("#userScore").html(userScore)
+        }
+
+        if (userScore > randNumber) {
+            losses= losses+= 1;
+            console.log(losses)
+            $("#loseTally").html(losses)
+            userScore = 0;
+            $("#userScore").html(userScore)
+        }
+            
+        });
 
 });
